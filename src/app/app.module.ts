d864b0d6ab7fdc5610df831532a34ae221f36a8d
savio <savioanjos2020@gmail.com>
2023-04-05 09:10:18 -0300

    Mudando login para o diretório de pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+
+// Components
 import { HeaderComponent } from './components/header/header.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ListComponent } from './components/list/list.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
 
+//Pages
+import { LoginComponent } from './pages/login/login.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+
     HeaderComponent,
+    LoginComponent,
     RegisterComponent,
     ListComponent,
-    ItemListComponent
+    ItemListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
